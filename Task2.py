4c5fd2393ae73356316b13e789459d2f5cc2b686
Devang Bhatnagar <gamefun7102@gmail.com>
2024-10-17 10:31:15 -0700

    Prints each individual user

--- Task2.py
+++ Task2.py
@@ -1,20 +1,42 @@
-import bcrypt
+# from bcrypt import *
 
 def main():
-    print("Hello World")
-    # example password 
-    password = 'password123'
+    # print("Hello World")
+    # # example password 
+    # password = 'password123'
     
-    # converting password to array of bytes 
-    bytes = password.encode('utf-8') 
+    # # converting password to array of bytes 
+    # bytes = password.encode('utf-8') 
     
-    # generating the salt 
-    salt = bcrypt.gensalt() 
+    # # generating the salt 
+    # salt = bcrypt.gensalt() 
     
-    # Hashing the password 
-    hash = bcrypt.hashpw(bytes, salt) 
+    # # Hashing the password 
+    # hash = bcrypt.hashpw(bytes, salt) 
     
-    print(hash)
+    # print(hash)
 
-if 'name' == "__main__":
-    main()
+    shadow = ""
+
+    with open("shadow.txt", "r") as file:
+    	shadow = file.readlines()
+    for j in range(0, len(shadow)):
+	    shadow_split = shadow[j].split('$')
+	    user = shadow_split[0]
+	    algorithm = shadow_split[1]
+	    workFactor = shadow_split[2]
+	    saltAndHash = shadow_split[3]
+	    salt = ""
+	    hashValue = ""
+	    for i in range(0, 22):
+	    	salt+=saltAndHash[i]
+	    for i in range(22, len(saltAndHash)):
+	    	hashValue+=saltAndHash[i]
+	    print("User : ", user)
+	    print("algorithm : ", algorithm)
+	    print("workFactor : ", workFactor)
+	    print("salt: ", salt)
+	    print("hash: ", hashValue)
+
+
+main()
